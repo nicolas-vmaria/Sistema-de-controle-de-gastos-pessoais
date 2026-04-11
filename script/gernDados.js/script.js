c5402e0bb16167299d12465afbe3717366c1cb57
Caio Triches <boing.caio@gmail.com>
2026-04-11 12:09:43 -0300

    Aplicando modal e categorias

--- script/gernDados.js/script.js
+++ script/gernDados.js/script.js
@@ -1,31 +1,105 @@
-const valorInput = document.getElementById('valor');
-const gastosButton = document.getElementById('gasto');
-const enviarButton = document.getElementById('enviar');
-const inputGatos = document.getElementById('inputGastos');
+const salarioInput = document.getElementById('valorSalario');
+const gastosButton = document.getElementById('btnGasto');
+const btnSalario = document.getElementById('btnSalario');
+const inputGastos = document.getElementById('gastosInput');
 const listElement = document.getElementById('listaGastos');
 const saldo = document.getElementById('saldo');
+const openButtons = document.querySelectorAll('.open-modal');
+const closeButtons = document.querySelectorAll('.close-modal');
+const selectElement = document.getElementById('cat-gastos');
+let modal1 = document.getElementById('modal-1');
+let modal2 = document.getElementById('modal-2');
 
-let salario = 0;
+
+let salario = 0;    
 let gastosTotais = 0;
 let gastosArray = [];
+let categorias = [
+    "Aluguel/Moradia",
+    "Supermercado",
+    "Água e Luz",
+    "Internet e Telefone",
+    "Transporte/Combustível",
+    "Farmácia",
+    "Consultas Médicas",
+    "Escola/Faculdade",
+    "Restaurantes e Delivery",
+    "Streaming",
+    "Academia",
+    "Roupas e Calçados",
+    "Impostos e Taxas",
+    "Pets",
+    "Presentes",
+    "Outros"
+];
 
-function adicionarSalario(valor){
-    confirm("Deseja adicionar esse valor ao salário?");
-    if(confirm){
-        salario += valor;
-        saldo.textContent = salario.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
-    }else {
-        alert("Valor não adicionado ao salário.");
-    }
-
-    return salario;
+function limparModal2() {
+    inputGastos.value = '';
+    document.getElementById('dataGastos').value = '';
+    document.getElementById('cat-gastos').value = 'Escolha uma categoria';
 }
 
-enviarButton.addEventListener('click', () => {
-    const valor = parseFloat(valorInput.value);
+function limparModal1() {
+    salarioInput.value = '';
+}
+
+categorias.forEach(categoria => {
+    const option = document.createElement('option');
+    option.value = categoria;
+    option.textContent = categoria;
+    selectElement.appendChild(option);
+}); 
+
+
+
+openButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        
+        const modalId = button.getAttribute('data-modal');
+
+        const modal = document.getElementById(modalId);
+
+        modal.showModal();
+    });
+});
+
+closeButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modalId = button.getAttribute('data-modal');
+
+        const modal = document.getElementById(modalId);
+
+        modal.close();
+        limparModal2();
+        limparModal1();
+    });
+});
+
+function adicionarSalario(valor){
+    let resposta = confirm("Deseja adicionar esse valor ao salário?");
+
+    if(resposta){
+        salario += valor;
+        saldo.textContent = salario.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+        return true;
+    }else {
+        alert("Valor não adicionado ao salário.");
+        return false;
+    }
+}
+
+btnSalario.addEventListener('click', () => {
+    const valor = parseFloat(salarioInput.value);
+
     if (!isNaN(valor)) {
-        adicionarSalario(valor);
-        valorInput.value = '';
+        let sucesso = adicionarSalario(valor);
+        if(sucesso){
+            alert("Salário atualizado com sucesso!");
+            modal1.close();
+            limparModal1();
+        }
+        
+
     } else {
         alert('Por favor, insira um número válido.');
     }
@@ -34,15 +108,17 @@ enviarButton.addEventListener('click', () => {
 function renderGastos(){
     listElement.innerHTML = ''
 
-    gastosArray.map((todo)=>{
+    gastosArray.forEach((todo, index)=>{
     
     let liElement = document.createElement("li")
     let gastosText = document.createTextNode(`R$ ${todo.valor}`)
-    let posicao = gastosArray.indexOf(todo)
+    let posicao = index;
 
  
     let dateformatada = new Date(todo.data + "T00:00:00").toLocaleDateString('pt-BR', { day: '2-digit', month: '2-digit', year: 'numeric' });
     let dateText = document.createTextNode(` - ${dateformatada}`)
+
+    let catgeoriaText = document.createTextNode(` - ${todo.categoria}`)
     
 
     // let linkElement = document.createElement("a")
@@ -64,59 +140,65 @@ function renderGastos(){
     // liElement.appendChild(linkElement)
     liElement.appendChild(gastosText)
     liElement.appendChild(dateText)
+    liElement.appendChild(catgeoriaText)
     // liElement.appendChild(editElement)
     listElement.appendChild(liElement)
 })
 }
 
-gastosButton.onclick = adicionarGastosLista;
+gastosButton.addEventListener('click', adicionarGastos);
 
-function adicionarGastosLista(){
-    if(inputGatos.value === ''){
+function adicionarGastos(){
+    let newGastos = parseFloat(inputGastos.value);
+    let dataGastos = document.getElementById('dataGastos').value;
+    let saldofuturo = salario - newGastos;
+    let categoriaGastos = document.getElementById('cat-gastos').value;
+
+    if(inputGastos.value === ''){
         alert("Digite uma quantia para adicionar aos gastos.");
         return false;
-    }else{
-        let newGAstos = inputGatos.value;
-        let dataGastos = document.getElementById('dataGastos').value;
-
-        if(!dataGastos){
-            alert("Por favor, selecione uma data para o gasto.");
-            return false;
-        }
-        gastosArray.push({
-            valor: newGAstos,
-            data: document.getElementById('dataGastos').value
-        });
-
-        adicionarGastos(parseFloat(newGAstos));
-        renderGastos()
+    }else if(isNaN(newGastos)){
+        alert("Digite um número válido para os gastos.");
+        return false;
+    }else if(dataGastos === ''){
+        alert("Selecione uma data para o gasto.");
+        return false;
+    }else if(categoriaGastos === 'Escolha uma categoria'){
+        alert("Selecione uma categoria para o gasto.");
+        return false;
     }
 
-    
-}
+      // Depois que o usuário preencher os campos, o sistema verifica se o gasto fará o saldo ficar negativo. 
+      // Se sim, ele exibe um alerta e pergunta se o usuário deseja continuar adicionando esse gasto. 
+      // Se o usuário confirmar, o gasto é adicionado mesmo com o saldo negativo, e um alerta adicional é exibido para avisar sobre a situação. 
+      // Se o usuário cancelar, o gasto não é adicionado e uma mensagem de confirmação é exibida.
 
-function adicionarGastos(valor){
-   
-    let saldofuturo = salario - valor;
-    
-    console.log(saldofuturo);
     if(saldofuturo <= 0){
         let resposta = confirm("Atenção: Seus gastos excederam seu salário! Deseja continuar adicionando esse gasto?");
         if(resposta){
-            salario -= valor;
-            gastosTotais += valor;
+            salario -= newGastos;
+            gastosTotais += newGastos;
             alert("Seu saldo está negativo! Considere revisar seus gastos.");
         }else{
-            salario += valor; 
             alert("Gasto não adicionado.");
-            return;
+            modal2.close();
+            limparModal2();
+            return false;
         }
     }else{
-        salario -= valor;
-        gastosTotais += valor;
+        alert("Gasto adicionado com sucesso!");
+        salario -= newGastos;
+        gastosTotais += newGastos;
+        
     }
 
-    saldo.textContent = (salario - gastosTotais).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+    gastosArray.push({valor: newGastos, data: dataGastos, categoria: categoriaGastos});
+    limparModal2();
+    modal2.close();
+    renderGastos();
+    saldo.textContent = salario.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+
+    
 }
 
 
